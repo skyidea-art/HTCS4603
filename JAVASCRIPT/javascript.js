6b40810f2d37e640506b6b1e2cf51cb38be70d96
Mel37-Cee <65927178+Mel37-Cee@users.noreply.github.com>
2020-06-04 13:52:06 +1200

    Changes

--- JAVASCRIPT/javascript.js
+++ JAVASCRIPT/javascript.js
@@ -23,56 +23,174 @@ ul_element_effect_2 = document.getElementById("ul_element_s_2");
 ul_element_effect_3 = document.getElementById("ul_element_s_3");
 ul_element_effect_4 = document.getElementById("ul_element_s_4");
 ul_element_effect_5 = document.getElementById("ul_element_s_5");
+font_bold_0 = document.getElementById("font_deco_bold_0");
+font_bold_1 = document.getElementById("font_deco_bold_1");
+font_bold_2 = document.getElementById("font_deco_bold_2");
+font_bold_3 = document.getElementById("font_deco_bold_3");
+font_bold_4 = document.getElementById("font_deco_bold_4");
+font_bold_5 = document.getElementById("font_deco_bold_5");
+// font normals
+font_normal_0 = document.getElementById("font_deco_normal_0");
+font_normal_1 = document.getElementById("font_deco_normal_1");
+font_normal_2 = document.getElementById("font_deco_normal_2");
+font_normal_3 = document.getElementById("font_deco_normal_3");
+font_normal_4 = document.getElementById("font_deco_normal_4");
+font_normal_5 = document.getElementById("font_deco_normal_5");
+font_normal_6 = document.getElementById("font_deco_normal_6");
+font_normal_7 = document.getElementById("font_deco_normal_7");
+font_normal_8 = document.getElementById("font_deco_normal_8");
+font_normal_9 = document.getElementById("font_deco_normal_9");
+font_normal_10 = document.getElementById("font_deco_normal_10");
 // variable lists.
 // javascript must be defined with different effects.
+chat_size_0 = document.getElementById("chat_open");
+slide_fixed_0 = document.getElementById("slide_button_s");
+
 function ul_element_effectActiv_0()
 {
-	ul_element_effect_0.style.background = "grey";
+	ul_element_effect_0.style.background = "white";
+	font_bold_0.style.color = "black";
+	font_effect_0.style.borderBottomColor = "black";
+	font_effect_1.style.borderBottomColor = "black";
+	font_effect_2.style.borderBottomColor = "black";
+	font_effect_3.style.borderBottomColor = "black";
+	font_normal_0.style.color ="black";
+	font_normal_1.style.color ="black";
+	font_normal_2.style.color ="black";
+
 }
 function ul_element_effectDeac_0()
 {
 	ul_element_effect_0.style.background = "red";
+	font_bold_0.style.color = "white"; 
+	font_effect_0.style.borderBottomColor = "white";
+	font_effect_1.style.borderBottomColor = "white";
+	font_effect_2.style.borderBottomColor = "white";
+	font_effect_3.style.borderBottomColor = "white";
+	font_normal_0.style.color ="white";
+	font_normal_1.style.color ="white";
+	font_normal_2.style.color ="white";
+	
 }
 function ul_element_effectActiv_1()
 {
-	ul_element_effect_1.style.background = "grey";
+	ul_element_effect_1.style.background = "white";
+	font_bold_1.style.color = "black";
+	font_effect_4.style.borderBottomColor = "black";
+	font_effect_5.style.borderBottomColor = "black";
+	font_effect_6.style.borderBottomColor = "black";
+	font_normal_3.style.color = "black";
+	font_normal_4.style.color = "black";
 }
 function ul_element_effectDeac_1()
 {
-	ul_element_effect_1.style.background = "blue";
+	ul_element_effect_1.style.background = "#ff5733";
+	font_bold_1.style.color = "white"; 
+	font_effect_4.style.borderBottomColor = "white";
+	font_effect_5.style.borderBottomColor = "white";
+	font_effect_6.style.borderBottomColor = "white";
+	font_normal_3.style.color = "white";
+	font_normal_4.style.color = "white";
 }
 function ul_element_effectActiv_2()
 {
-	ul_element_effect_2.style.background = "grey";
+	ul_element_effect_2.style.background = "white";
+	font_bold_2.style.color = "black";
+	font_effect_7.style.borderBottomColor = "black";
+	font_effect_8.style.borderBottomColor = "black";
+	font_effect_9.style.borderBottomColor = "black";
+	font_normal_5.style.color = "black";
+	font_normal_6.style.color = "black";
+
 }
 function ul_element_effectDeac_2()
 {
-	ul_element_effect_2.style.background = "green";
+	ul_element_effect_2.style.background = "#3355ff";
+	font_bold_2.style.color = "white"; 
+	font_effect_7.style.borderBottomColor = "white";
+	font_effect_8.style.borderBottomColor = "white";
+	font_effect_9.style.borderBottomColor = "white";
+	font_normal_5.style.color = "white";
+	font_normal_6.style.color = "white";
 }
 function ul_element_effectActiv_3()
 {
-	ul_element_effect_3.style.background = "grey";
+	ul_element_effect_3.style.background = "white";
+	font_bold_3.style.color = "black";
+	font_effect_10.style.borderBottomColor = "black";
+	font_effect_11.style.borderBottomColor = "black";
+	font_effect_12.style.borderBottomColor = "black";
+	font_normal_7.style.color = "black";
+	font_normal_8.style.color = "black";
+
 }
 function ul_element_effectDeac_3()
 {
-	ul_element_effect_3.style.background = "yellow";
+	ul_element_effect_3.style.background = "#6eff33";
+	font_bold_3.style.color = "white"; 
+	font_effect_10.style.borderBottomColor = "white";
+	font_effect_11.style.borderBottomColor = "white";
+	font_effect_12.style.borderBottomColor = "white";
+	font_normal_7.style.color = "white";
+	font_normal_8.style.color = "white";
 }
 function ul_element_effectActiv_4()
 {
-	ul_element_effect_4.style.background = "grey";
+	ul_element_effect_4.style.background = "white";
+	font_bold_4.style.color = "black";
+	font_effect_13.style.borderBottomColor = "black";
+	font_effect_14.style.borderBottomColor = "black";
+	font_effect_15.style.borderBottomColor = "black";
+	font_normal_9.style.color = "black";
+	font_normal_10.style.color = "black";
 }
 function ul_element_effectDeac_4()
 {
-	ul_element_effect_4.style.background = "orange";
+	ul_element_effect_4.style.background = "#ffef2b";
+	font_bold_4.style.color = "white"; 
+	font_effect_13.style.borderBottomColor = "white";
+	font_effect_14.style.borderBottomColor = "white";
+	font_effect_15.style.borderBottomColor = "white";
+	font_normal_9.style.color = "white";
+	font_normal_10.style.color = "white";
+
 }
 function ul_element_effectActiv_5()
 {
-	ul_element_effect_5.style.background = "grey";
+	ul_element_effect_5.style.background = "white";
+	font_bold_5.style.color = "black";
+	font_effect_16.style.borderBottomColor ="black";
 }
 function ul_element_effectDeac_5()
 {
-	ul_element_effect_5.style.background = "black";
+	ul_element_effect_5.style.background = "#565656";
+	font_bold_5.style.color = "white"; 
+	font_effect_16.style.borderBottomColor ="white";
 }
+
+//the end of the functions 
+var a = -1;
+function open_closechat()
+{
+	a+=1;
+	if(a == 0)
+	{
+		chat_size_0.style.width = "0px";
+		//alert(a);
+	}
+	if(a == 1)
+	{
+		chat_size_0.style.width = "400px";
+		//alert(a);
+	}
+	if(a == 2)
+	{
+		chat_size_0.style.width = "0px";
+		a = 0;
+		//alert(a);
+	}
+}
+
 var x = -1;
 function slidebar_button()
 {
@@ -80,6 +198,8 @@ function slidebar_button()
 	//alert(x);
 	if(x == 0)
 	{
+		//button for slide.
+		slide_fixed_0.style.marginLeft = "5px";
 		slide_comp_0.style.width = "0px";
 		font_effect_0.style.width = "0px";
 		font_effect_0.style.borderBottomColor ="black";
@@ -115,10 +235,30 @@ function slidebar_button()
 		font_effect_15.style.borderBottomColor ="black";
 		font_effect_16.style.width = "0px";
 		font_effect_16.style.borderBottomColor ="black";
+		//font colors and properties
+		font_bold_0.style.color = "black";
+		font_bold_1.style.color = "black";
+		font_bold_2.style.color = "black";
+		font_bold_3.style.color = "black";
+		font_bold_4.style.color = "black";
+		font_bold_5.style.color = "black";
+		//font colors and normals
+		font_normal_0.style.color = "black";
+		font_normal_1.style.color = "black";
+		font_normal_2.style.color = "black";
+		font_normal_3.style.color = "black";
+		font_normal_4.style.color = "black";
+		font_normal_5.style.color = "black";
+		font_normal_6.style.color = "black";
+		font_normal_7.style.color = "black";
+		font_normal_8.style.color = "black";
+		font_normal_9.style.color = "black";
+		font_normal_10.style.color = "black";
 		
 	}
 	if(x == 1)
 	{
+		slide_fixed_0.style.marginLeft = "200px";
 		slide_comp_0.style.width = "200px";
 		font_effect_0.style.width = "100px";
 		font_effect_0.style.borderBottomColor ="white";
@@ -154,12 +294,30 @@ function slidebar_button()
 		font_effect_15.style.borderBottomColor ="white";
 		font_effect_16.style.width = "100px";
 		font_effect_16.style.borderBottomColor ="white";
-		
-		
+		//font colors and properties
+		font_bold_0.style.color = "white";
+		font_bold_1.style.color = "white";
+		font_bold_2.style.color = "white";
+		font_bold_3.style.color = "white";
+		font_bold_4.style.color = "white";
+		font_bold_5.style.color = "white";
+		//font colors and normals
+		font_normal_0.style.color = "white";
+		font_normal_1.style.color = "white";
+		font_normal_2.style.color = "white";
+		font_normal_3.style.color = "white";
+		font_normal_4.style.color = "white";
+		font_normal_5.style.color = "white";
+		font_normal_6.style.color = "white";
+		font_normal_7.style.color = "white";
+		font_normal_8.style.color = "white";
+		font_normal_9.style.color = "white";
+		font_normal_10.style.color = "white";
 	}
 	if(x == 2)
 	{
 		x = 0;
+		slide_fixed_0.style.marginLeft = "5px";
 		slide_comp_0.style.width = "0px";
 		font_effect_0.style.width = "0px";
 		font_effect_0.style.borderBottomColor ="black";
@@ -195,5 +353,24 @@ function slidebar_button()
 		font_effect_15.style.borderBottomColor ="black";
 		font_effect_16.style.width = "0px";
 		font_effect_16.style.borderBottomColor ="black";
+		//font colors and properties
+		font_bold_0.style.color = "black";
+		font_bold_1.style.color = "black";
+		font_bold_2.style.color = "black";
+		font_bold_3.style.color = "black";
+		font_bold_4.style.color = "black";
+		font_bold_5.style.color = "black";
+		//font colors and normals
+		font_normal_0.style.color = "black";
+		font_normal_1.style.color = "black";
+		font_normal_2.style.color = "black";
+		font_normal_3.style.color = "black";
+		font_normal_4.style.color = "black";
+		font_normal_5.style.color = "black";
+		font_normal_6.style.color = "black";
+		font_normal_7.style.color = "black";
+		font_normal_8.style.color = "black";
+		font_normal_9.style.color = "black";
+		font_normal_10.style.color = "black";
 	}
 }
